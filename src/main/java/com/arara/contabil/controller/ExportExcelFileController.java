e5b044d942af1e06958647b2717141000e6d5f15
Joao Menezes <joaopedro6br@gmail.com>
2024-05-05 22:59:54 -0300

    add h2 console link to home page

--- src/main/java/com/arara/contabil/controller/ExportExcelFileController.java
+++ src/main/java/com/arara/contabil/controller/ExportExcelFileController.java
@@ -4,16 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
