897c42eecdf7c72bbe46c24d5adb62d98c48eec4
Joao Menezes <joaopedro6br@gmail.com>
2024-05-18 18:02:04 -0300

    add unique constraint on EscolaPrograma Entity

--- src/main/java/com/arara/contabil/model/EscolaPrograma.java
+++ src/main/java/com/arara/contabil/model/EscolaPrograma.java
@@ -18,10 +18,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "nomePrograma", "exercicio", "organization" }) })
 public class EscolaPrograma {
 
 	@Id
