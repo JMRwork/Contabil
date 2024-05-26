1ed8e21cac16c3a28c7624ca00e57f84177c54cd
José Marco Ribeiro de Menezes <josemarcor12@gmail.com>
2024-05-25 21:40:40 -0300

    update edit dados cadastrais

--- src/main/java/com/arara/contabil/controller/EscolaDadosCadastraisController.java
+++ src/main/java/com/arara/contabil/controller/EscolaDadosCadastraisController.java
@@ -10,10 +10,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.arara.contabil.dto.EditEscolaDadosCadastraisDto;
 import com.arara.contabil.model.EscolaDadosCadastrais;
-import com.arara.contabil.model.Organization;
 import com.arara.contabil.service.ConverterService;
 import com.arara.contabil.service.EscolaDadosCadastraisService;
 
@@ -29,20 +29,26 @@ public class EscolaDadosCadastraisController {
 	@PreAuthorize("hasRole('ADMIN') || principal.organizationIds.contains(#organizationId)")
 	@GetMapping("/organizations/{organizationId}/dados-cadastrais")
 	public String showDadosCadastrais(@PathVariable("organizationId") Long organizationId, Model model) {
-		model.addAttribute("cadastro", escolaDadosCadastraisService.findById(organizationId).get());
+		var cadastro = escolaDadosCadastraisService.findById(organizationId);
+		if (cadastro.isPresent()) {
+			model.addAttribute("cadastro", cadastro.get());
+		}	
 		return "view-dados-cadastrais";
 	}
 	
 	@PreAuthorize("hasRole('ADMIN') || principal.organizationIds.contains(#organizationId)")
 	@GetMapping("/organizations/{organizationId}/dados-cadastrais/edit")
-	public String editDadosCadastrais(@PathVariable("organizationId") Long organizationId, 
+	public String editDadosCadastrais(@PathVariable("organizationId") Long organizationId,
+			@RequestParam(value="create", required = false, defaultValue = "false") boolean create,
 			EditEscolaDadosCadastraisDto editEscolaDadosCadastraisDto, 
 			Model model, 
 			BindingResult result) {
 		Optional<EscolaDadosCadastrais> escolaDadosCadastrais = escolaDadosCadastraisService.findById(organizationId);
 		if (escolaDadosCadastrais.isEmpty()) {
-			ObjectError error = new ObjectError("globalError", "Dados Cadastrais da organização de id " + organizationId + " não existe.");
-			result.addError(error);
+			if(!create) {
+				ObjectError error = new ObjectError("globalError", "Dados Cadastrais da organização de id " + organizationId + " não existe.");
+				result.addError(error);
+				}
 		} else {
 			model.addAttribute("EditEscolaDadosCadastraisDto", converterService
 					.convertEscolaDadosCadastraisModelToEditEscolaDadosCadastraisDto(escolaDadosCadastrais.get(), editEscolaDadosCadastraisDto));
